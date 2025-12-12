b06b29c6d32f0e550622b23b17f3c8496ea2624c
unknown <priyanshushikarwal@gmail.com>
2025-12-13 03:42:56 +0530

    Fix: Hardcode Sanity ID in client lib

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,8 +3,8 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-    projectId: import.meta.env.VITE_SANITY_PROJECT_ID || "fmrcedhu",
-    dataset: import.meta.env.VITE_SANITY_DATASET || "production",
+    projectId: "fmrcedhu",
+    dataset: "production",
     useCdn: true, // set to `false` to bypass the edge cache
     apiVersion: "2024-01-01", // use current date (YYYY-MM-DD) to target the latest API version
 });
